85eba74f1af033a5be88c034036910ca328d5af5
bdicaprio <a.live@163.com>
2020-06-01 19:45:34 +0800

    完成字段名字修改

--- app01/views.py
+++ app01/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from anaconda_navigator.utils.py3compat import request
-from django.http import  HttpResponse
 from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
 
 # Create your views here.
 def Index(request):
     return render_to_response('index.html')
+    
     
